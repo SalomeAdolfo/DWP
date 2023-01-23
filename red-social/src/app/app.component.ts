1ed8d231919aaa942a4ac064ebd2b6116a430b9f
Adolfo Angel Salomé Hernández <al222010588@gmail.com>
2023-01-23 13:24:51 -0600

    Práctica de *ng

--- red-social/src/app/app.component.ts
+++ red-social/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'red-social';
+  mostrarMensaje = true;
+  materias: string[] =[
+    'Gestión del proceso de desarrollo de software',
+    'Aplcaciones web progresivas',
+    'Integradora',
+    'Desarrollo móvil integral',
+    'Optativa 1: Creación de videojuegos',
+    'Negociación empresarial',
+    'Inbglés IX'
+  ];
+  contador = 0;
 }
